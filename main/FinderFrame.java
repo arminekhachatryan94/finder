32df6b2d25784bd4273846c62971aaef83395003
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-09-27 19:23:56 -0700

    add tabs to frame

--- main/FinderFrame.java
+++ main/FinderFrame.java
@@ -1,15 +1,56 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class FinderFrame extends JFrame {
     private static final int FRAME_WIDTH = 500;
     private static final int FRAME_HEIGHT = 400;
 
+    public JPanel tabs;
+
     public FinderFrame() {
         setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+        setBackground(Color.white);
+        createTabs();
+        getContentPane().add(tabs, BorderLayout.PAGE_START);
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void createTabs(){
+        JLabel find = new JLabel("Find");
+        JLabel replace = new JLabel("Replace");
+        JLabel findInFiles = new JLabel("Find in Files");
+
+        find.setOpaque(true);
+        find.setForeground(Color.black);
+        find.setBackground(Color.white);
+        find.setBorder(new EmptyBorder(2,5,2,5));
+        replace.setOpaque(true);
+        replace.setForeground(Color.black);
+        replace.setBackground(Color.white);
+        replace.setBorder(new EmptyBorder(2,5,2,5));
+        findInFiles.setOpaque(true);
+        findInFiles.setForeground(Color.black);
+        findInFiles.setBackground(Color.white);
+        findInFiles.setBorder(new EmptyBorder(2,5,2,5));
+
+        FlowLayout flow = new FlowLayout(FlowLayout.LEFT);
+        flow.setHgap(0);
+        flow.setVgap(0);
+        
+        tabs = new JPanel();
+        tabs.setLayout(flow);
+        tabs.setSize(500, 20);
+        tabs.add(find);
+        tabs.add(replace);
+        tabs.add(findInFiles);
     }
 }
