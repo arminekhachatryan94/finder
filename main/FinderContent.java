a019065ce40e6452e6e5ce4249d5b63ff5125ebc
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-09-30 20:38:36 -0700

    add directory textarea to find tab

--- main/FinderContent.java
+++ main/FinderContent.java
@@ -17,6 +17,7 @@ public class FinderContent extends JPanel {
     public int WIDTH;
     public int HEIGHT;
     public static JTextArea text;
+    public static JTextArea location;
 
     FinderContent(String type, int width, int height){
         this.type = type;
@@ -28,19 +29,19 @@ public class FinderContent extends JPanel {
 
         if( type == "Find" ){
             createFindPanel();
-        } /*else if( type == "Replace" ){
+        } else if( type == "Replace" ){
             createReplacePanel();
         } else if( type == "Find in Files" ){
             createFindInFilesPanel();
-        }*/
+        }
     }
 
     public void createFindPanel(){
         // row 1
-        JLabel str = new JLabel("Find what:");
-        str.setBackground(Color.red);
-        str.setBounds(20, 15, 70, 10);
-        add(str);
+        JLabel findWhat = new JLabel("Find what:");
+        findWhat.setBackground(Color.red);
+        findWhat.setBounds(20, 10, 70, 20);
+        add(findWhat);
 
         text = new JTextArea(1, 15);
         text.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -48,7 +49,18 @@ public class FinderContent extends JPanel {
         text.setBounds(100, 10, 200, 20);
         add(text);
 
+        // row 2
+        JLabel findWhere = new JLabel("Directory:");
+        findWhere.setBackground(Color.red);
+        findWhere.setBounds(20, 35, 70, 20);
+        add(findWhere);
 
+        location = new JTextArea(1, 15);
+        location.setBorder(BorderFactory.createLineBorder(Color.black));
+        location.setBackground(Color.white);
+        location.setBounds(100, 35, 200, 20);
+        add(location);
+        
         // buttons
         JButton findNextBtn = new JButton("Find Next");
         JButton countBtn = new JButton("Count");
@@ -73,5 +85,14 @@ public class FinderContent extends JPanel {
         add(findAllCurrentDocBtn);
         add(closeBtn);
     }
+
+    public void createReplacePanel() {
+        ;
+    }
+
+    public void createFindInFilesPanel(){
+        ;
+    }
+
 
 }
