1e8446bc5080d85d104a924fc6a0eec951a51057
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-10-09 15:31:43 -0700

    find word in directory

--- main/FinderSwingWorker.java
+++ main/FinderSwingWorker.java
@@ -1,12 +1,18 @@
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.concurrent.CompletableFuture;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
@@ -29,7 +35,10 @@ class FinderSwingWorker extends SwingWorker<Integer, String> {
         this.match[0] = match[0];
         this.match[1] = match[1];
         this.frame = new JFrame();
-        this.frame.setPreferredSize(new Dimension(400, 500));
+        this.frame.setPreferredSize(new Dimension(400, 400));
+        this.panel = new JPanel();
+        this.panel.setPreferredSize(new Dimension(400, 400));
+        this.panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
     }
 
     @Override
@@ -44,21 +53,79 @@ class FinderSwingWorker extends SwingWorker<Integer, String> {
         return count;
     }
 
-
     @Override
     protected void process(List<String> files) {
         for(String s : files) {
             File f = new File(s);
             System.out.println(s);
+            ArrayList<String> founds = findWord(f.getAbsolutePath());
+
+            BorderLayout borderLayout = new BorderLayout(10, 0);
+            if(founds.size() > 0 ){
+                JLabel l = new JLabel();
+                l.setLayout(borderLayout);
+                l.setText(s);
+                l.setSize(400, 10);
+                panel.add(l);
+                for(String found : founds ){
+                    JLabel label = new JLabel();
+                    label.setLayout(borderLayout);
+                    label.setText(found);
+                    label.setSize(400, 10);
+                    panel.add(label);
+                }
+            }
         }
     }
 
     @Override
     protected void done() {
+        JScrollPane scroller = new JScrollPane(this.panel);
+        scroller.setAutoscrolls(true);
+
+        this.frame.add(this.panel, BorderLayout.PAGE_END);
+        this.frame.add(scroller, BorderLayout.CENTER);
         this.frame.setVisible(true);
-        frame.setResizable(false);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.frame.setResizable(false);
+        this.frame.pack();
+        this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+
+    public ArrayList<String> findWord(String location){
+        ArrayList<String> found = new ArrayList<String>();
+        int count = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(location)))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                count++;
+                if( this.match[0].isSelected() && this.match[1].isSelected() ){
+                    String regex = ".*\\b" + this.find.getText() + "\\b.*";
+                    if(line.matches(regex)){
+                        found.add(new String("Line" + count + ": " + line));
+                    }
+                } else if( this.match[0].isSelected() ){
+                    String regex = ".*\\b" + this.find.getText().toLowerCase() + "\\b.*";
+                    if(line.toLowerCase().matches(regex)){
+                        found.add(new String("Line" + count + ": " + line));
+                    }
+                } else if( this.match[1].isSelected() ){
+                    if(line.contains(this.find.getText())){
+                        System.out.println(line);
+                        found.add(new String("Line " + count + ": " + line));
+                    }
+                } else {
+                    String _word = this.find.getText().toLowerCase();
+                    String _line = line.toLowerCase();
+                    if(_line.contains(_word)){
+                        System.out.println(line);
+                        found.add(new String("Line " + count + ": " + line));
+                    }
+                }
+            }
+        } catch( Exception e ){
+            System.out.println(e.toString());
+        }
+        return found;
     }
 
     void subDirectories(File d){
